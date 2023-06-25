fb065322459b76df71efa1d3ef3d22c0d87ed791
BrettWalker93 <45851235+BrettWalker93@users.noreply.github.com>
2023-06-25 05:23:40 -0500

    Create solution.cpp

---
+++ LeetCode/Hard Problems/Basic Calculator/solution.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int calculate(string s) {
+
+        vector<int> val, signs;
+        int sign = 1;
+        int ans = 0;
+        int v = 0;
+        int n = s.size();
+        for (int i = 0; i < n; i++) {
+            char c = s[i];
+            if (isalnum(c)) {
+                v = v * 10 + (c - '0');
+            }
+            else if (c == '+' or c == '-') {
+                ans += v * sign;
+                v = 0;
+                sign = 1;
+                if (c == '-') sign = -1;
+            }
+            else if (c == '(') {
+                val.push_back(ans);
+                signs.push_back(sign);
+                sign = 1;
+                ans = 0;
+            }
+            else if (c == ')') {
+                ans += sign * v;
+                ans *= signs.back();
+                ans += val.back();
+                signs.pop_back();
+                val.pop_back();
+                v = 0;
+
+            }
+        }
+
+        return ans + v * sign;
+
+    }
+
+};
